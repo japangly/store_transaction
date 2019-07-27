17857095dfb61ed918bd68807f62fa58351d1414
Japang-LY <japangly@gmail.com>
2019-07-27 14:54:32 +0700

    flutter_staggered_grid_view

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({
@@ -17,8 +18,21 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('store_transaction'),
       ),
       body: Center(
-        child: Container(
-          child: Text('store_transaction'),
+        child: StaggeredGridView.countBuilder(
+          crossAxisCount: 4,
+          itemCount: 8,
+          itemBuilder: (BuildContext context, int index) => new Container(
+              color: Colors.green,
+              child: new Center(
+                child: new CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: new Text('$index'),
+                ),
+              )),
+          staggeredTileBuilder: (int index) =>
+              new StaggeredTile.count(2, index.isEven ? 2 : 1),
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
         ),
       ),
     );
