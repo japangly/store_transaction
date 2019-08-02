035279e6d113466716868c6628d18c342b9c8cab
hourlee99 <oukchannlyhour@gmail.com>
2019-08-02 17:43:29 +0700

    receipt dialog

--- lib/dialog/stock_dialog.dart
+++ lib/dialog/stock_dialog.dart
@@ -3,7 +3,7 @@ import 'package:store_transaction/themes/helpers/fonts.dart';
 
 import '../env.dart';
 
-class ReceiptDialog extends StatelessWidget {
+class StockDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -14,7 +14,7 @@ class ReceiptDialog extends StatelessWidget {
           borderRadius: BorderRadius.all(Radius.circular(10.0))),
       content: SingleChildScrollView(
         child: Container(
-          width: Environment().getWidth(width: 35),
+          width: Environment().getWidth(width: 30),
           height: Environment().getHeight(height: 40),
           child: Column(
             children: <Widget>[
@@ -26,7 +26,7 @@ class ReceiptDialog extends StatelessWidget {
                       child: Column(
                         children: <Widget>[
                           Text(
-                            "Purchase Details",
+                            "Service Stock Check Out Details",
                             style: TextStyle(
                                 fontWeight: FontWeight.bold, fontSize: 25),
                             textAlign: TextAlign.center,
@@ -37,43 +37,28 @@ class ReceiptDialog extends StatelessWidget {
                             textAlign: TextAlign.center,
                           ),
                           Text(
-                            "Consumer No. 000001",
+                            "Employee No. 000001",
                             style: font20Black,
                             textAlign: TextAlign.center,
                           ),
                         ],
                       ),
                     ),
-                    Column(
-                      children: <Widget>[
-                        Table(
-                          children: [
-                            _buildTableRow("No.,Description, Qty, Subtotal"),
-                            _buildTableRow("1.,Sunsilk, 20, \$200.00"),
-                            _buildTableRow("2.,Head&Shoulder, 10, \$20.00"),
-                            _buildTableRow("3.,Pantene, 40, \$35.00"),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              new Container(
-                  margin: const EdgeInsets.only(left: 10.0, right: 20.0),
-                  child: Divider(
-                    color: Colors.black,
-                    height: 36,
-                  )),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Text(
-                      '\$35.00',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 25.0),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Table(
+                            children: [
+                              _buildTableRow("No,Description, Qty"),
+                              _buildTableRow("1., Sunsilk, 20"),
+                              _buildTableRow("2.,Head&Shoulder, 10"),
+                              _buildTableRow("3.,Pantene, 40"),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
